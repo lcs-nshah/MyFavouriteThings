b6540f2723564bf638d96a702c78677735c7efaf
Neil Shah <nshah@lcs.on.ca>
2024-12-11 11:38:44 -0500

    Initial Commit

---
+++ MyFavouriteThings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFavouriteThings
+//
+//  Created by Neil Shah on 2024-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFavouriteThings/MyFavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavouriteThingsApp.swift
+//  MyFavouriteThings
+//
+//  Created by Neil Shah on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
